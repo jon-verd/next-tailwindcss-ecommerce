4bd4640206ce41e0f20e9b584b92beae6600f6f1
jonv02 <96271886+jon-verd@users.noreply.github.com>
2022-11-15 17:26:10 -0700

    Added a new component named Layout which functions as a wrapper for all pages such as the home page. Also set up ESLint reccomended checking to find issues with my code!

---
+++ components/Layout.js
@@ -0,0 +1,37 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import React from 'react';
+
+export default function Layout({ title, children }) {
+  return (
+    <>
+      <Head>
+        <title>{title ? title + ' - Vicios' : 'Vicios'}</title>
+        <meta name="description" content="Ecommerce Site" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className="flex min-h-screen flex-col justify-between">
+        <header>
+          <nav className="flex h-12 items-center px-4 justify-between shadow-md">
+            <Link className="text-lg font-bold" href="/">
+              Vicios
+            </Link>
+            <div>
+              <Link className="p-2" href="/cart">
+                Cart
+              </Link>
+              <Link className="p-2" href="/login">
+                Login
+              </Link>
+            </div>
+          </nav>
+        </header>
+        <main className="container m-auto mt-4 px-4">{children}</main>
+        <footer className="flex h-10 shadow-inner justify-center items-center">
+          <p>Copyright © 2022 Vicios</p>
+        </footer>
+      </div>
+    </>
+  );
+}
