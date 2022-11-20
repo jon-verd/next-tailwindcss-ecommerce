6ca63e57aaae0bb6a69c9f6346f86fe0b170238d
Jonathan Verduzco <96271886+jon-verd@users.noreply.github.com>
2022-11-20 16:02:40 -0700

    halfway through my lesson on adding data and images of products to be listed on the site. Really enjoying this lesson so far.

--- utils/data.js
+++ utils/data.js
@@ -6,11 +6,76 @@ const data = {
             category : 'Shirts',
             image : '/images/shirt1.jpg',
             price : 70,
-        brand : 'nike',
-        rating : 4.5,
-        numReviews : 8,
-        countInStock : 20,
-        description : "A popular shirt.",
-        }
-    ]
-}   
+            brand : 'nike',
+            rating : 4.5,
+            numReviews : 8,
+            countInStock : 20,
+            description : "A popular shirt.",
+        },
+        {
+            name : 'Fit shirt',
+            slug : 'fit-shirt',
+            category : 'Shirts',
+            image : '/images/shirt2.jpg',
+            price : 80,
+            brand : 'Adidas',
+            rating : 3.2,
+            numReviews : 10,
+            countInStock : 20,
+            description : "A popular shirt.", 
+        },
+        {
+            name : 'Slim Shirt',
+            slug : 'slim-shirt',
+            category : 'Shirts',
+            image : '/images/shirt3.jpg',
+            price : 90,
+            brand : 'Raymond',
+            rating : 4.5,
+            numReviews : 3,
+            countInStock : 20,
+            description : "A popular shirt.", 
+        },
+
+        {
+            name : 'Golf Pants',
+            slug : 'golf-pants',
+            category : 'Pants',
+            image : '/images/pants1.jpg',
+            price : 90,
+            brand : 'Oliver',
+            rating : 2.9,
+            numReviews : 13,
+            countInStock : 20,
+            description : "Some Golf Pants."
+        },
+        {
+            name : 'fit Pants',
+            slug : 'fit-pants',
+            category : 'Pants',
+            image : '/images/pants2.jpg',
+            price : 95,
+            brand : 'Zara',
+            rating : 3.5,
+            numReviews : 7,
+            countInStock : 20,
+            description : "A popular pair."
+        },
+        {
+            name : 'Classic Pants',
+            slug : 'classic-pants',
+            category : 'Pants',
+            image : '/images/pants3.jpg',
+            price : 75,
+            brand : 'Casely',
+            rating : 2.4,
+            numReviews : 14,
+            countInStock : 20,
+            description : "Some Golf Pants."
+        },
+        
+
+    ],
+};
+
+export default data;
