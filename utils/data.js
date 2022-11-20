09a70e710416dc1113cc087d5ba129ec0dc1131c
Jonathan Verduzco <96271886+jon-verd@users.noreply.github.com>
2022-11-20 15:39:50 -0700

    adding some data

---
+++ utils/data.js
@@ -0,0 +1,16 @@
+const data = {
+    products: [
+        {
+            name : 'Free shirt',
+            slug : 'free-shirt',
+            category : 'Shirts',
+            image : '/images/shirt1.jpg',
+            price : 70,
+        brand : 'nike',
+        rating : 4.5,
+        numReviews : 8,
+        countInStock : 20,
+        description : "A popular shirt.",
+        }
+    ]
+}   
